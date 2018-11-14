eeed0c1d5399d1b1529f93afc55622b9efe204ba
Holger Schulz <holger.schulz@durham.ac.uk>
2018-11-14 09:43:33 -0600

    Move read functions to tools and make available when loading module

--- apprentice/__init__.py
+++ apprentice/__init__.py
@@ -1,3 +1,4 @@
 from apprentice import *
 from apprentice.rationalapproximation   import RationalApproximation
 from apprentice.polynomialapproximation import PolynomialApproximation
+from apprentice.tools import readData, readApprentice
--- apprentice/tools.py
+++ apprentice/tools.py
@@ -25,3 +25,32 @@ def maxOrder(N, dim):
     while comb(dim + omax+1, omax+1) + 1 <= N: # The '+1' stands for a order 0 polynomial's dof
         omax+=1
     return omax
+
+
+def readData(fname, delimiter=","):
+    """
+    Read CSV formatted data. The last column is interpreted as
+    function values while all other columns are considered
+    parameter points.
+    """
+    import os
+    if not os.path.exists(fname): raise Exception("File {} not found".format(fname))
+    import numpy as np
+    D = np.loadtxt(fname, delimiter=delimiter)
+    X=D[:,0:-1]
+    Y=D[:,-1]
+    return X, Y
+
+def readApprentice(fname):
+    """
+    Read an apprentice JSON file. We abuse try except here to
+    figure out whether it's a rational or polynomial approximation.
+    """
+    import apprentice
+    import os
+    if not os.path.exists(fname): raise Exception("File {} not found".format(fname))
+    try:
+        app = apprentice.RationalApproximation(fname=fname)
+    except:
+        app = apprentice.PolynomialApproximation(fname=fname)
+    return app
