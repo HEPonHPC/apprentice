15109b96779bca84aadff5844a04e8a86ca74a84
Mohan Krishnamoorthy <mkrishnamoorthy@mkrishnamoorthy.mcs.anl.gov>
2018-11-15 09:37:47 -0600

    Adding benchmark data. Added check to mkBenchmarkData.sh to not generate data if the data file already exists.

--- benchmarkdata/scripts/mkBenchmarkData.sh
+++ benchmarkdata/scripts/mkBenchmarkData.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-for i in {7..8}; do python mkBenchmarkData.py -c -o ../f${i}.txt                        -n 1000   -r 0   -f ${i};done
-# for i in {1..6}; do python mkBenchmarkData.py -c -o ../f${i}_noise_0.1.txt              -n 1000   -r 0.1 -f ${i};done
-# for i in {1..6}; do python mkBenchmarkData.py -c -o ../f${i}_noise_0.5.txt              -n 1000   -r 0.5 -f ${i};done
-# for i in {1..6}; do python mkBenchmarkData.py -c -o ../f${i}_test.txt           -s 9999 -n 100000 -r 0   -f ${i};done
-# for i in {1..6}; do python mkBenchmarkData.py -c -o ../f${i}_noise_0.1_test.txt -s 9999 -n 100000 -r 0.1 -f ${i};done
-# for i in {1..6}; do python mkBenchmarkData.py -c -o ../f${i}_noise_0.5_test.txt -s 9999 -n 100000 -r 0.5 -f ${i};done
+for i in {1..8}; do if [ ! -e "../f${i}.txt" ];                then python mkBenchmarkData.py -c -o ../f${i}.txt                        -n 1000   -r 0   -f ${i}; fi done
+for i in {1..8}; do if [ ! -e "../f${i}_noise_0.1.txt" ];      then python mkBenchmarkData.py -c -o ../f${i}_noise_0.1.txt              -n 1000   -r 0.1 -f ${i}; fi done
+for i in {1..8}; do if [ ! -e "../f${i}_noise_0.5.txt" ];      then python mkBenchmarkData.py -c -o ../f${i}_noise_0.5.txt              -n 1000   -r 0.5 -f ${i}; fi done
+for i in {1..8}; do if [ ! -e "../f${i}_test.txt" ];           then python mkBenchmarkData.py -c -o ../f${i}_test.txt           -s 9999 -n 100000 -r 0   -f ${i}; fi done
+for i in {1..8}; do if [ ! -e "../f${i}_noise_0.1_test.txt" ]; then python mkBenchmarkData.py -c -o ../f${i}_noise_0.1_test.txt -s 9999 -n 100000 -r 0.1 -f ${i}; fi done
+for i in {1..8}; do if [ ! -e "../f${i}_noise_0.5_test.txt" ]; then python mkBenchmarkData.py -c -o ../f${i}_noise_0.5_test.txt -s 9999 -n 100000 -r 0.5 -f ${i}; fi done
