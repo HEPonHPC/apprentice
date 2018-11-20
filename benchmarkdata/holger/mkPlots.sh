ae58b66cbee31edec66c0b19a54b843116c785d2
Mohan Krishnamoorthy <mkrishnamoorthy@anl.gov>
2018-11-20 17:27:03 -0600

    bug fix in mkPlots

--- benchmarkdata/holger/mkPlots.sh
+++ benchmarkdata/holger/mkPlots.sh
@@ -2,7 +2,7 @@
 
 # ./mkPlots.sh ../f1_test.txt plots_f1_s1_t100 ../scripts/JSON/f1_s1_*t100.json
 #
-# Arguments: 
+# Arguments:
 #     1. input file with test points
 #     2. output directory for plots (will be created)
 #     all others: json files
@@ -16,9 +16,9 @@ for fin in ${@:3}
 do
     btest=`basename $ftest`
     bin=`basename $fin`
-    fout="$OUTDIR/${btest%%.*}_${bin%%.*}.png"
+    fout="$OUTDIR/${btest%.*}_${bin%.*}.png"
     python ../scripts/bm-plot2D.py $fin $ftest -n 1 -o $fout
-done 
+done
 
 cd $OUTDIR
 for n in {0..4};do convert  *n$n*png +append row_n$n.png;done
