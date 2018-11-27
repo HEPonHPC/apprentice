a1d476ac0e2838f0ae2b42a2a4b6690f039ae4b9
Mohan Krishnamoorthy <mkrishnamoorthy@anl.gov>
2018-11-27 10:45:21 -0600

    Added subplots for error plots with different training sizes

--- benchmarkdata/scripts/bm-plot2D.py
+++ benchmarkdata/scripts/bm-plot2D.py
@@ -33,7 +33,10 @@ def plotError(f_test, f_out, norm=1, *f_rapp):
     testSize = len(X_test[:,0])
     # error that maps average error to m,n on x and y axis respectively
     import numpy as np
-    error_m_n = np.zeros(shape=(4,4))
+    error_m_n_all = np.zeros(shape=(4,4))
+    error_m_n_1x = np.zeros(shape=(4,4))
+    error_m_n_2x = np.zeros(shape=(4,4))
+    error_m_n_1k = np.zeros(shape=(4,4))
 
     for i in range(len(f_rapp[0])):
         # print(f_rapp[0][i])
@@ -42,17 +45,17 @@ def plotError(f_test, f_out, norm=1, *f_rapp):
         if norm == 2: res = [(R(x)-Y_test[num])**2 for num, x in enumerate(X_test)]
         m = R.m
         n = R.n
-        error_m_n[m-1][n-1] = error_m_n[m-1][n-1] + sum(res)/testSize
+        addTerm = sum(res)/testSize
+        if R.trainingsize == R.M + R.N:
+            error_m_n_1x[m-1][n-1] = error_m_n_1x[m-1][n-1] + addTerm
+        elif R.trainingsize == 2*(R.M + R.N):
+            error_m_n_2x[m-1][n-1] = error_m_n_2x[m-1][n-1] + addTerm
+        elif R.trainingsize == 1000:
+            error_m_n_1k[m-1][n-1] = error_m_n_1k[m-1][n-1] + addTerm
+        else:
+            raise Exception("Something is wrong here. Incorrect training size used")
+        error_m_n_all[m-1][n-1] = error_m_n_all[m-1][n-1] + addTerm
 
-    # for i in range(len(error_m_n)):
-    #     for j in range(len(error_m_n[i])):
-    #         print(error_m_n[i][j])
-
-    # from math import floor, ceil
-    # minE = floor(np.amin(error_m_n))
-    # maxE = ceil(np.amax(error_m_n))
-    # print(minE)
-    # print(maxE)
     import matplotlib as mpl
     import matplotlib.pyplot as plt
     mpl.rc('text', usetex = True)
@@ -62,14 +65,14 @@ def plotError(f_test, f_out, norm=1, *f_rapp):
     X,Y = np.meshgrid(range(1,5), range(1,5))
 
     f, axarr = plt.subplots(2,2, sharex=True, sharey=True)
-
-    sc1 = axarr[0,0].scatter(X,Y, marker = 's', s=400, c = error_m_n, cmap = cmapname, alpha = 1)
+    markersize = 400
+    sc1 = axarr[0,0].scatter(X,Y, marker = 's', s=markersize, c = error_m_n_all, cmap = cmapname, alpha = 1)
     axarr[0,0].set_title('All training size')
-    sc2 = axarr[0,1].scatter(X,Y, marker = 's', s=400, c = error_m_n, cmap = cmapname, alpha = 1)
+    sc2 = axarr[0,1].scatter(X,Y, marker = 's', s=markersize, c = error_m_n_1x, cmap = cmapname, alpha = 1)
     axarr[0,1].set_title('Training size = 1x')
-    sc3 = axarr[1,0].scatter(X,Y, marker = 's', s=400, c = error_m_n, cmap = cmapname, alpha = 1)
+    sc3 = axarr[1,0].scatter(X,Y, marker = 's', s=markersize, c = error_m_n_2x, cmap = cmapname, alpha = 1)
     axarr[1,0].set_title('Training size = 2x')
-    sc4 = axarr[1,1].scatter(X,Y, marker = 's', s=400, c = error_m_n, cmap = cmapname, alpha = 1)
+    sc4 = axarr[1,1].scatter(X,Y, marker = 's', s=markersize, c = error_m_n_1k, cmap = cmapname, alpha = 1)
     axarr[1,1].set_title('Training size = 1000')
 
     for ax in axarr.flat:
@@ -78,7 +81,8 @@ def plotError(f_test, f_out, norm=1, *f_rapp):
         ax.label_outer()
     b=f.colorbar(sc1,ax=axarr.ravel().tolist(), shrink=0.95)
     b.set_label("Error = $\\frac{\\left|\\left|f - \\frac{p^m}{q^n}\\right|\\right|_%i}{%i}$"%(norm,testSize))
-    plt.show()
+    # plt.show()
+    plt.savefig(f_out)
 
 
 if __name__=="__main__":
