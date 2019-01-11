2c26d4a8e293e7fa6b50cb506831ec5f5b6379c9
Holger Schulz <holger.schulz@durham.ac.uk>
2019-01-11 17:10:39 -0600

    bugfix in string handling

--- workflow/approx_from_h5.py
+++ workflow/approx_from_h5.py
@@ -23,26 +23,27 @@ if __name__ == "__main__":
 
 
     # This reads the data for all bins
+    DATA = apprentice.tools.readH5(sys.argv[1], [])
+    idx = [i for i in range(len(DATA))]
 
-    # DATA = apprentice.tools.readH5(sys.argv[1], [])
-    # idx = [i for i in range(len(DATA))]
-
-    # This reads the data for a selection of bins
-    idx = [0,1,2,5,7,20,44]
-    DATA = apprentice.tools.readH5(sys.argv[1], idx)
+    # # This reads the data for a selection of bins
+    # idx = [0,1,2,5,7,8,9,14,20,44]
+    # DATA = apprentice.tools.readH5(sys.argv[1], idx)
 
     # Note: the idx is needed to make a connection to experimental data
 
     ras = []
+    scl = []
+    S = apprentice.Scaler(DATA[0][0]) # Let's assume that all X are the same for simplicity
     for X, Y in  DATA:
-        ras.append(apprentice.RationalApproximation(X, Y, order=(3,1)))
+        ras.append(apprentice.RationalApproximation(S.scaledPoints, Y, order=(3,1)))
+    S.save("{}.scaler".format(sys.argv[2]))
 
     # This reads the unique identifiers of the bins
     with h5py.File(sys.argv[1], "r") as f:  binids = f.get("index")[idx]
 
-    # jsonify
-    JD = { str(x) : y.asDict for x, y in zip(binids, ras) }
-
+    # jsonify # The decode deals with the conversion of byte string atributes to utf-8
+    JD = { x.decode() : y.asDict for x, y in zip(binids, ras) }
 
     import json
     with open(sys.argv[2], "w") as f: json.dump(JD, f)
