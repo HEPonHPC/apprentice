7ae666fe51c3c98bb79d64346098c57e963e5c89
Holger Schulz <holger.schulz@durham.ac.uk>
2019-02-18 14:56:28 -0600

    Import monomial stuff when calling import apprentice

--- apprentice/__init__.py
+++ apprentice/__init__.py
@@ -4,3 +4,4 @@ from apprentice.polynomialapproximation import PolynomialApproximation
 from apprentice.rationalapproximationSIP import RationalApproximationSIP
 from apprentice.tools import readData, readApprentice
 from apprentice.scaler import Scaler
+from apprentice.monomial import monomialStructure
