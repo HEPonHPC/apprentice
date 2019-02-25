68608552065d49b56ff283ce4323ad7a56c33ff8
Mohan Krishnamoorthy <mkrishnamoorthy@anl.gov>
2019-02-25 15:46:09 -0600

    Unchange

--- experiments/runrappsip.sh
+++ experiments/runrappsip.sh
@@ -1,30 +1,32 @@
 #!/bin/bash
-# ./runrappsip.sh ../benchmarkdata/f12.txt f12_2x 0 2 0 2 2x
-# ./runrappsip.sh ../workflow/data/DM_6D.h5 6D_2x 0 2 0 2 2x
+# ./runrappsip.sh ../benchmarkdata/f12.txt f12 2 2 2x
+# ./runrappsip.sh ../workflow/data/DM_6D.h5 6D 0 0 2x
 programname=$0
 function usage {
-    echo "usage: $programname infile fndesc mmin mmax nmin nmax ts"
+    echo "usage: $programname infile fndesc mmax nmax ts"
     exit 1
 }
 
-if [ $# -lt 7 ]; then
+if [ $# -lt 5 ]; then
     usage
 fi
 
 infile=$1;
 fndesc=$2;
-mmin=$3;
-mmax=$4;
-nmin=$5;
-nmax=$6;
-ts=$7;
+mmax=$3;
+nmax=$4;
+ts=$5;
 
 mkdir -p $fndesc/out;
 mkdir -p $fndesc/log/consolelog;
 # python runrappsip.py ../benchmarkdata/f12.txt f12 2 2 1x 6d
-for pdeg in $(seq $mmin $mmax); do
-    for qdeg in $(seq $nmin $nmax); do
+for pdeg in $(seq 0 $mmax); do
+    for qdeg in $(seq 0 $nmax); do
       colsolelog=$fndesc"/log/consolelog/"$fndesc"_p"$pdeg"_q"$qdeg"_ts"$ts".log";
+      if [ -f "$colsolelog" ]
+      then
+        continue;
+      fi
       echo $consolelog;
       nohup python runrappsip.py $infile $fndesc $pdeg $qdeg $ts $fndesc >$colsolelog 2>&1 &
       # python runrappsip.py $infile $fndesc $pdeg $qdeg $ts $fndesc >$colsolelog 2>&1
