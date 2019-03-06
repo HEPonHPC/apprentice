83c5cd0b169d2be0d59463fc317eff7d8d06d4bc
Mohan Krishnamoorthy <mkrishnamoorthy@anl.gov>
2019-03-06 13:55:14 -0600

    Adding ability to plot rapp and compare with rappsip

--- experiments/plot2Dsurface.py
+++ experiments/plot2Dsurface.py
@@ -1,5 +1,5 @@
 import numpy as np
-from apprentice import RationalApproximationSIP, PolynomialApproximation
+from apprentice import RationalApproximationSIP, RationalApproximation, PolynomialApproximation
 from sklearn.model_selection import KFold
 from apprentice import tools, readData
 import os
@@ -62,8 +62,12 @@ def plot2Dsurface(infile,testfile,folder, desc,bottom_or_all):
         rappsip = RationalApproximationSIP(datastore)
         Y_pred = rappsip.predictOverArray(X_test)
     except:
-        papp = PolynomialApproximation(initDict=datastore)
-        Y_pred = np.array([papp(x) for x in X_test])
+        if(n==0):
+            papp = PolynomialApproximation(initDict=datastore)
+            Y_pred = np.array([papp(x) for x in X_test])
+        else:
+            rapp = RationalApproximation(initDict=datastore)
+            Y_pred = np.array([rapp(x) for x in X_test])
 
     ax = fig.add_subplot(2, 2, 2, projection='3d')
     ax.plot3D(X_test[:,0],X_test[:,1], Y_pred[:],"b.")
@@ -82,10 +86,15 @@ def plot2Dsurface(infile,testfile,folder, desc,bottom_or_all):
     l1 = np.sum(np.absolute(Y_pred-Y_test))
     l2 = np.sqrt(np.sum((Y_pred-Y_test)**2))
     linf = np.max(np.absolute(Y_pred-Y_test))
+    if(linf>10**3): print("FOUND===>%f"%(linf))
     try:
         nnz = tools.numNonZeroCoeff(rappsip,nnzthreshold)
     except:
-        nnz = tools.numNonZeroCoeff(papp,nnzthreshold)
+        if(n==0):
+            nnz = tools.numNonZeroCoeff(papp,nnzthreshold)
+        else:
+            nnz = tools.numNonZeroCoeff(rapp,nnzthreshold)
+
     # print(l2)
     # print(nnz)
     # print(l2/nnz)
@@ -93,10 +102,49 @@ def plot2Dsurface(infile,testfile,folder, desc,bottom_or_all):
     fig.suptitle("%s. m = %d, n = %d, ts = %d (%s). l1 = %.4f, l2 = %.4f, linf = %.4f, nnz = %d, l2/nnz = %f"%(desc,m,n,trainingsize,ts,l1,l2,linf,nnz,l2/nnz))
 
     plt.savefig(outfilepng)
+    plt.close('all')
 
 # python plot2Dsurface.py f21_2x/out/f21_2x_p12_q12_ts2x.json ../benchmarkdata/f21_test.txt f21_2x f21_2x all
 
 if __name__ == "__main__":
+
+    # import apprentice
+    # name = "f14"
+    # noisestr = "_noisepct10-3"
+    # # noisestr = ""
+    # trainfile = "../benchmarkdata/"+name+noisestr+".txt"
+    # X, Y = readData(trainfile)
+    # folder = "poletest"
+    # if not os.path.exists(folder):
+    #     os.mkdir(folder)
+    # for m in range(1,6):
+    #     for n in range(1,6):
+    #         trainingsize = 2 * tools.numCoeffsRapp(2,(m,n))
+    #         i_train = [i for i in range(trainingsize)]
+    #         rapp = apprentice.RationalApproximation(X[i_train],Y[i_train],order=(m,n), strategy=1)
+    #
+    #         # rappsip  = apprentice.RationalApproximationSIP(X[i_train],Y[i_train],m=m,n=n,trainingscale="Cp",
+    #         #                     strategy=0,roboptstrategy = 'msbarontime',fitstrategy = 'filter',localoptsolver = 'scipy')
+    #         # rappsip.save(folder+"/rappsip.json")
+    #
+    #
+    #         rapp.save(folder+"/rapp.json")
+    #
+    #         testfile = "../benchmarkdata/"+name+"_test.txt"
+    #
+    #         plot2Dsurface(folder+"/rapp.json", testfile, folder, name+noisestr+"_rapp","all")
+    #
+    #         rappsipfile = "%s%s_2x/out/%s%s_2x_p%d_q%d_ts2x.json"%(name,noisestr,name,noisestr,m,n)
+    #         plot2Dsurface(rappsipfile, testfile, folder, name+noisestr+"_rappsip","all")
+    #
+    #         # plot2Dsurface(folder+"/rappsip.json", testfile, folder, name+noisestr+"_rappsip","all")
+    #
+    # exit(1)
+
+
+
+
+
     import os, sys
     if len(sys.argv)!=6:
         print("Usage: {} infile testfile folder fndesc bottom_or_all".format(sys.argv[0]))
