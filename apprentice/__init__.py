83e9a7360955cfb2665d5a170e338989b92b3cfa
Holger Schulz <holger.schulz@durham.ac.uk>
2019-03-13 09:11:00 -0500

    Also import ONB things

--- apprentice/__init__.py
+++ apprentice/__init__.py
@@ -1,7 +1,9 @@
 from apprentice import *
 from apprentice.rationalapproximation   import RationalApproximation
+from apprentice.rationalapproximationONB   import RationalApproximationONB
 from apprentice.polynomialapproximation import PolynomialApproximation
 from apprentice.rationalapproximationSIP import RationalApproximationSIP
 from apprentice.tools import readData, readApprentice
 from apprentice.scaler import Scaler
+from apprentice.onb import ONB
 from apprentice.monomial import monomialStructure
