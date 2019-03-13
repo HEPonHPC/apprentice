16c1cbef3d47398cd9c2e10323a5652b0bcb9e08
Holger Schulz <holger.schulz@durham.ac.uk>
2019-03-13 09:12:37 -0500

    Accelerate onb with numba

--- apprentice/onb.py
+++ apprentice/onb.py
@@ -1,5 +1,84 @@
 import numpy as np
 
+
+from numba import jit
+
+@jit
+def fast_calc(_X, M, Mdof):
+    """
+    Stieltjes ONB procedure
+    M ... highest order for polynomials --- this determines the matrix structure
+    """
+
+
+    K   = np.atleast_2d(_X).shape[0]
+    dim = np.atleast_2d(_X).shape[1]
+
+
+    Q = np.atleast_2d(np.zeros((K, Mdof)))    # ONB
+    R = np.atleast_2d(np.zeros((Mdof, Mdof))) # Recurrence matrix--- stores projections, required to evaluate polynomialsx
+
+    recInfoInd = np.zeros(Mdof, dtype=np.int)
+    recInfoVar = np.zeros(Mdof, dtype=np.int)
+
+
+    R[0][0]=np.sqrt(K)             # initial (constant vector)
+    Q[:,0] = np.ones((K))/R[0][0]  # insert into matrix
+
+    i = 1 # Start algorithm at second vector
+    ind = np.zeros(dim+1, dtype=np.int) # Bookkeeping,initially just a bunch of zeros
+
+    # Iterate over degrees
+    for m in range(1,M+1):
+        # Iterate over variable
+        for n in range(dim):
+            indnn = i
+            # Stieltjes/Gram-Schmidt loop.
+            #
+            # TODO:  This is MGS.  Would CGS be better for
+            # parallelization/efficiency?
+            # print("m= {} n={} ||  {} -- {}".format(m,n,ind[n], ind[-1]+1))
+            for j in range(ind[n], ind[-1]+1):
+                Q[:,i] = _X[:,n] * Q[:,j]
+                recInfoInd[i] = j;
+                recInfoVar[i] = n;
+
+                # Orthogonalize.
+                for k in range(i):
+                    R[k][i] = np.dot(Q[:,k],Q[:,i])
+                    Q[:,i]  = Q[:,i] - R[k][i]*Q[:,k]
+                R[i][i]=np.linalg.norm(Q[:,i])
+                Q[:,i] = Q[:,i]/R[i][i]
+
+                # Re-orthogonalize.
+                for k in range(i):
+                    Q[:,i] = Q[:,i] - np.dot(Q[:,k], Q[:,i])*Q[:,k]
+                Q[:,i] = Q[:,i]/np.linalg.norm(Q[:,i])
+                i+=1
+            ind[n] = indnn
+        ind[-1]=i-1
+
+    return Q, R, recInfoInd, recInfoVar
+
+
+
+@jit
+def fast_recurrence(X, dof, R, recVar, recInd):
+    """
+    Build the recurrence matrix for point X.
+    X   ... point of interest
+    dof ... degrees of freedom
+    """
+    Q = np.zeros(dof)
+    # TODO: make this more efficient
+    Q[0] = 1./R[0][0]
+    for i in range(1, dof):
+        Q[i]  = X[recVar[i]] * Q[recInd[i]]
+        for j in range(i): # Note: this was the trouble maker when translating from matlab
+            Q[i] -= Q[j]*R[j][i]
+        Q[i] /= R[i][i]
+    return Q
+
 def maxOrder(N, dim):
     """
     Utility function to find highest order polynomial
@@ -60,48 +139,7 @@ class ONB(object):
         from scipy.special import comb
         Mdof = int(comb(self.dim + M, M))
 
-        Q = np.atleast_2d(np.zeros((K, Mdof)))    # ONB
-        R = np.atleast_2d(np.zeros((Mdof, Mdof))) # Recurrence matrix--- stores projections, required to evaluate polynomialsx
-
-        recInfoInd = np.zeros(Mdof, dtype=np.int)
-        recInfoVar = np.zeros(Mdof, dtype=np.int)
-
-
-        R[0][0]=np.sqrt(K)             # initial (constant vector)
-        Q[:,0] = np.ones((K))/R[0][0]  # insert into matrix
-
-        i = 1 # Start algorithm at second vector
-        ind = np.zeros(self.dim+1, dtype=np.int) # Bookkeeping,initially just a bunch of zeros
-
-        # Iterate of degrees
-        for m in range(1,M+1):
-            # Iterate over variable
-            for n in range(self.dim):
-                indnn = i
-                # Stieltjes/Gram-Schmidt loop.
-                #
-                # TODO:  This is MGS.  Would CGS be better for
-                # parallelization/efficiency?
-                # print("m= {} n={} ||  {} -- {}".format(m,n,ind[n], ind[-1]+1))
-                for j in range(ind[n], ind[-1]+1):
-                    Q[:,i] = self._X[:,n] * Q[:,j]
-                    recInfoInd[i] = j;
-                    recInfoVar[i] = n;
-
-                    # Orthogonalize. 
-                    for k in range(i):
-                        R[k][i] = np.dot(Q[:,k],Q[:,i])
-                        Q[:,i]  = Q[:,i] - R[k][i]*Q[:,k]
-                    R[i][i]=np.linalg.norm(Q[:,i])
-                    Q[:,i] = Q[:,i]/R[i][i]
-
-                    # Re-orthogonalize.
-                    for k in range(i):
-                        Q[:,i] = Q[:,i] - np.dot(Q[:,k], Q[:,i])*Q[:,k]
-                    Q[:,i] = Q[:,i]/np.linalg.norm(Q[:,i])
-                    i+=1
-                ind[n] = indnn
-            ind[-1]=i-1
+        Q, R, recInfoInd, recInfoVar = fast_calc(self._X, M, Mdof)
 
         self._Q = Q
         self._R = R
@@ -129,16 +167,7 @@ class ONB(object):
         X   ... point of interest
         dof ... degrees of freedom
         """
-        import numpy as np
-        Q = np.zeros(dof)
-        # TODO: make this more efficient
-        Q[0] = 1./self._R[0][0]
-        for i in range(1, dof):
-            Q[i]  = X[self._recVar[i]] * Q[self._recInd[i]]
-            for j in range(i): # Note: this was the trouble maker when translating from matlab
-                Q[i] -= Q[j]*self._R[j][i]
-            Q[i] /= self._R[i][i]
-        return Q
+        return fast_recurrence(X,dof, self._R, self._recVar, self._recInd)
 
     @property
     def asDict(self):
@@ -176,10 +205,6 @@ class ONB(object):
 if __name__== "__main__":
 
     X=np.linspace(-1,1,40)
-    from pyraider import raider3
-    rs = raider3.RaiderSet(np.array([[x] for x in X]), tol=-1)
-
-
 
     aa= ONB(np.array([[x] for x in X]))
     aa._recurrence([X[0]], 6)
