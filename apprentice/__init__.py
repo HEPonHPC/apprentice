bd8287e3a81363026085014e766a8a788ffeb515
Holger Schulz <holger.schulz@durham.ac.uk>
2019-03-20 19:14:57 -0500

    Load utilities

--- apprentice/__init__.py
+++ apprentice/__init__.py
@@ -3,7 +3,8 @@ from apprentice.rationalapproximation   import RationalApproximation
 from apprentice.rationalapproximationONB   import RationalApproximationONB
 from apprentice.polynomialapproximation import PolynomialApproximation
 from apprentice.rationalapproximationSIP import RationalApproximationSIP
-from apprentice.tools import readData, readApprentice
+from apprentice.tools import readData, readApprentice, readApprox, readExpData
+from apprentice.weights import read_pointmatchers
 from apprentice.scaler import Scaler
 from apprentice.onb import ONB
 from apprentice.monomial import monomialStructure
