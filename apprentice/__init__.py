ddaa89917892a1e9c41ef898991e70cd3881fb38
Holger Schulz <holger.schulz@durham.ac.uk>
2019-04-09 08:34:11 -0500

    SIP ONB import

--- apprentice/__init__.py
+++ apprentice/__init__.py
@@ -3,6 +3,7 @@ from apprentice.rationalapproximation   import RationalApproximation
 from apprentice.rationalapproximationONB   import RationalApproximationONB
 from apprentice.polynomialapproximation import PolynomialApproximation
 from apprentice.rationalapproximationSIP import RationalApproximationSIP
+from apprentice.rationalapproximationSIPONB import RationalApproximationSIPONB
 from apprentice.tools import readData, readApprentice, readApprox, readExpData
 from apprentice.weights import read_pointmatchers
 from apprentice.scaler import Scaler
