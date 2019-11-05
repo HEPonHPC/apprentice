01c8a3bf9b0ace35f0a7c1f21f34d92571bf2a8d
Holger Schulz <holger.schulz@durham.ac.uk>
2019-11-05 14:10:02 -0600

    bump version

--- setup.py
+++ setup.py
@@ -1,23 +1,23 @@
 from setuptools import setup, find_packages
 setup(
   name = 'apprentice',
-  version = '0.4.0',
+  version = '0.5.1',
   description = 'The apprentice',
   url = 'https://xgitlab.cels.anl.gov/mkrishnamoorthy/apprentice',
   author = 'Mohan Krishnamoorthy, Holger Schulz',
   author_email = 'mkrishnamoorthy@anl.gov, hschulz@fnal.gov',
-  packages = find_packages(),
+   packages = find_packages(),
   include_package_data = True,
-  install_requires = [
-    'numpy',
-    'scipy',
-    'sklearn',
-    'numba',
-    'h5py'
-    # 'sobol',
-    # 'pyDOE'
-  ],
-  scripts=["bin/app-ls", "bin/app-tune", "bin/app-rox", "bin/app-nest", "bin/app-rox-parallel", "bin/app-predict"],
+ install_requires = [
+   'numpy',
+   'scipy',
+   'sklearn',
+   'numba',
+   'h5py'
+   # 'sobol',
+   # 'pyDOE'
+ ],
+  scripts=["bin/app-ls", "bin/app-tune", "bin/app-rox", "bin/app-nest", "bin/app-rox-parallel", "bin/app-predict", "etc/convertDat.py", "etc/simplecomp.py"],
   extras_require = {
   },
   entry_points = {
