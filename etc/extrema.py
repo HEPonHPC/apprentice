123b155aeb11b21e654226fa338e11e7052c51bf
Holger Schulz <holger.schulz@durham.ac.uk>
2019-11-25 14:51:36 -0600

    MPI capable script to find the minimum and maximum assumed by each bin in the interpolation domain. The results are stored in a new approximation file as attributed vmin and vmax

---
+++ etc/extrema.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+
+import apprentice as app
+import numpy as np
+from mpi4py import MPI
+
+
+
+if __name__ == "__main__":
+    import sys
+    from scipy import optimize
+    import time
+
+    comm = MPI.COMM_WORLD
+    size = comm.Get_size()
+    rank = comm.Get_rank()
+
+
+    from apprentice.tools import TuningObjective
+
+    import optparse, os, sys
+    op = optparse.OptionParser(usage=__doc__)
+    op.add_option("-v", "--debug", dest="DEBUG", action="store_true", default=False, help="Turn on some debug messages")
+    op.add_option("-o", dest="OUTPUT", default="approxwextremata.json", help="Output file (default: %default)")
+    op.add_option("-t", "--trials", dest="NTRIALS", type=int, default=100, help="Number of points to sample to find startpoint (default: %default)")
+    op.add_option("-r", "--restart", dest="NRESTART", type=int, default=1, help="Number of restarts (default: %default)")
+    op.add_option("--seed", dest="SEED", type=int, default=1234, help="Random number seed (default: %default)")
+    op.add_option("-i", "--inplace", dest="INPLACE", default=False, action='store_true', help="Overwrite input file (default: %default)")
+    op.add_option("--filter", dest="FILTER", default=False, action='store_true', help="Filter bins that do no envelope data (default: %default)")
+    op.add_option("--filter-n", dest="FILTERN", default=10, type=int, help="Number of multistarts when determining fmin/max (default: %default)")
+    op.add_option("-g", "--grad", dest="USEGRAD", default=False, action='store_true', help="Use gradients in minimisation (default: %default)")
+    opts, args = op.parse_args()
+
+    np.random.seed(opts.SEED)
+    binids, RA = app.tools.readApprox(args[0], set_structures = True)
+
+    rankWork = app.tools.chunkIt([i for i in range(len(binids))], size) if rank==0 else []
+    rankWork = comm.scatter(rankWork, root=0)
+    import time
+    t0=time.time()
+
+
+    FMIN = [(binids[i], RA[i].fmin(opts.NTRIALS, use_grad=opts.USEGRAD)) for i in rankWork]
+    FMAX = [(binids[i], RA[i].fmax(opts.NTRIALS, use_grad=opts.USEGRAD)) for i in rankWork]
+    t1=time.time()
+
+    _FMIN = comm.gather(FMIN, root=0)
+    _FMAX = comm.gather(FMAX, root=0)
+    if rank==0:
+        _FMIN = [ x for sublist in _FMIN for x in sublist]
+        _FMAX = [ x for sublist in _FMAX for x in sublist]
+        for k, v in _FMIN:
+            i = binids.index(k)
+            RA[i]._vmin=v
+        for k, v in _FMAX:
+            i = binids.index(k)
+            RA[i]._vmax=v
+
+        import json
+        with open(args[0]) as f:
+            rd = json.load(f)
+            xmin = rd["__xmin"]
+            xmax = rd["__xmax"]
+
+        JD = {b : r.asDict for b,r in zip(binids, RA)}
+        JD["__xmin"] = xmin
+        JD["__xmax"] = xmax
+
+        if not opts.INPLACE:
+            with open(opts.OUTPUT, "w") as f: json.dump(JD, f, indent=4)
+            print("Done --- {} approximations written to {}".format(len(JD), opts.OUTPUT))
+        else:
+            with open(args[0], "w") as f: json.dump(JD, f, indent=4)
+            print("Done --- {} approximations written in place to {}".format(len(JD), args[0]))
