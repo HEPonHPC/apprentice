9cdfae6ea66e4c9e79adf77f2f0972392abe32fb
Mohan Krishnamoorthy <mkrishnamoorthy@anl.gov>
2020-01-24 09:19:18 -0600

    Make a copy of observables only in weights file

--- experiments/createcsvfromh5.py
+++ experiments/createcsvfromh5.py
@@ -1,7 +1,7 @@
 import apprentice as app
 import optparse, os, sys, h5py
 import numpy as np
-
+from shutil import copyfile
 
 if __name__ == "__main__":
 
@@ -9,8 +9,31 @@ if __name__ == "__main__":
     op.add_option("--log", dest="ISLOG", action='store_true', default=False,
                   help="input data is logarithmic --- affects how we filter (default: %default)")
     op.add_option("-o", dest="OUTPUT", default=None, help="Output folder (default: %default)")
+    op.add_option("-w", dest="WEIGHTS", default=None, help="Obervable file (default: %default)")
     opts, args = op.parse_args()
+
+    if os.path.exists(opts.OUTPUT):
+        uk = '_all'
+        if uk in opts.OUTPUT:
+            print("found {}".format(opts.OUTPUT))
+            if opts.WEIGHTS is not None:
+                weights = list(set(app.tools.readObs(opts.WEIGHTS)))
+                for i in range(len(weights)):
+                    weights[i] = weights[i].replace("_",'')
+                    weights[i] = weights[i].replace("/", '')
+                outdir2 = opts.OUTPUT.split(uk)[0]
+                os.makedirs(outdir2,exist_ok=True)
+                for file in os.listdir(opts.OUTPUT):
+                    if file.split('#')[0].replace('_','') in weights:
+                        pathin = os.path.join(opts.OUTPUT,file)
+                        pathout = os.path.join(outdir2,file)
+                        copyfile(pathin, pathout)
+            sys.exit(0)
+
     os.makedirs(opts.OUTPUT, exist_ok=True)
+
+
+
     binids = app.tools.readIndex(args[0])
 
     for num, b in enumerate(binids):
