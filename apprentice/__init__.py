5fea6b7383fc01f3688dc2e44cf9707bbb3b7bc0
Holger Schulz <hschulz@fnal.gov>
2020-03-30 16:17:55 -0500

    import tweaks

--- apprentice/__init__.py
+++ apprentice/__init__.py
@@ -1,13 +1,10 @@
 from apprentice import *
 from apprentice.rationalapproximation   import RationalApproximation
-from apprentice.rationalapproximationONB   import RationalApproximationONB
 from apprentice.polynomialapproximation import PolynomialApproximation
-from apprentice.rationalapproximationSIP import RationalApproximationSIP
 from apprentice.rationalapproximationSLSQP import RationalApproximationSLSQP
-from apprentice.rationalapproximationSIPONB import RationalApproximationSIPONB
+from apprentice.rationalapproximationSIP import RationalApproximationSIP
 from apprentice.tools import readData, readApprentice, readApprox, readExpData
 from apprentice.weights import read_pointmatchers
 from apprentice.scaler import Scaler
-from apprentice.onb import ONB
 from apprentice.monomial import monomialStructure
 from apprentice.appset import AppSet
