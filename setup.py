c0e653ec4944f027912c0fec36e4fe2151ff874f
Holger Schulz <hschulz@fnal.gov>
2020-04-24 06:46:11 -0500

    increment version, add more scripts to be installed

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
   name = 'apprentice',
-  version = '1.0.0',
+  version = '1.0.2',
   description = 'The apprentice',
   url = 'https://xgitlab.cels.anl.gov/mkrishnamoorthy/apprentice',
   author = 'Mohan Krishnamoorthy, Holger Schulz',
@@ -16,7 +16,7 @@ setup(
    'h5py',
    'mpi4py'
  ],
-  scripts=["bin/app-ls", "bin/app-tune2", "bin/app-build", "bin/app-predict", "bin/app-sample", "etc/convertData.py", "etc/convert.py", "etc/extrema.py"],
+  scripts=["bin/app-ls", "bin/app-tune2", "bin/app-build", "bin/app-predict", "bin/app-yoda2h5", "bin/app-sample", "etc/convertData.py", "etc/convert.py", "etc/extrema.py"],
   extras_require = {
   },
   entry_points = {
