301b4f17e4b8bb6d8521932902f978187657c457
Holger Schulz <hschulz@fnal.gov>
2020-05-04 13:24:54 -0500

    Add and install script that makes yoda files for envelope plotting

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
    'h5py',
    'mpi4py'
  ],
-  scripts=["bin/app-ls", "bin/app-tune2", "bin/app-build", "bin/app-predict", "bin/app-datadirtojson", "bin/app-yoda2h5", "bin/app-sample", "etc/extrema.py"],
+  scripts=["bin/app-ls", "bin/app-tune2", "bin/app-build", "bin/app-predict", "bin/app-datadirtojson", "bin/app-yoda2h5", "bin/app-yodaenvelope", "bin/app-sample", "etc/extrema.py"],
   extras_require = {
   },
   entry_points = {
