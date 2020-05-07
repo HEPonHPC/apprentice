5520cac8c42e3051efd21ea2f7b143572c9ab05c
Holger Schulz <hschulz@fnal.gov>
2020-05-07 13:38:48 -0500

    update setup and bump version

--- setup.py
+++ setup.py
@@ -1,20 +1,21 @@
 from setuptools import setup, find_packages
 setup(
-  name = 'apprentice',
-  version = '1.0.4',
+  name = 'pyapprentice',
+  version = '1.0.5',
   description = 'The apprentice',
-  url = 'https://xgitlab.cels.anl.gov/mkrishnamoorthy/apprentice',
+  url = 'https://computing.fnal.gov/hep-on-hpc/',
   author = 'Mohan Krishnamoorthy, Holger Schulz',
-  author_email = 'mkrishnamoorthy@anl.gov, hschulz@fnal.gov',
+  author_email = 'mkrishnamoorthy@anl.gov, iamholger@gmail.com',
    packages = find_packages(),
   include_package_data = True,
  install_requires = [
-   'numpy',
-   'scipy',
+   'numpy>=1.15.0',
+   'scipy>=1.3.0',
    'sklearn',
-   'numba',
-   'h5py',
-   'mpi4py'
+   'numba>=0.40.0',
+   'h5py>=2.8.0',
+   'mpi4py>=3.0.0',
+   'matplotlib>=3.0.0'
  ],
   scripts=["bin/app-ls", "bin/app-tune2", "bin/app-build", "bin/app-predict", "bin/app-datadirtojson", "bin/app-yoda2h5", "bin/app-yodaenvelope", "bin/app-sample", "etc/extrema.py"],
   extras_require = {
