7c3a9d35acca607604181e6a45c7ee7aff139221
Mohan Krishnamoorthy <mkrishnamoorthy@anl.gov>
2020-05-26 16:08:38 -0500

    Heteroschedastic GP

--- apprentice/GP.py
+++ apprentice/GP.py
@@ -8,7 +8,6 @@ import datetime
 from timeit import default_timer as timer
 import argparse
 
-
 class GaussianProcess():
     def __init__(self, *args, **kwargs):
         datafile = kwargs['DATAFILE']
@@ -21,13 +20,16 @@ class GaussianProcess():
         self.buildtype = kwargs['BUILDTYPE']
 
         if self.buildtype=="data":
+            self.SEED  = kwargs['SEED']
+            self.STOPCOND = kwargs['STOPCOND']
             self.kernel = kwargs['KERNEL']
             self.obsname = kwargs['OBS']
             # self.nprocess = kwargs['NPROCESS']
             self.nrestart = kwargs['NRESTART']
             self.keepout = kwargs['KEEPOUT']/100
             os.makedirs(kwargs['OUTDIR'],exist_ok=True)
-            self.outfile = os.path.join(kwargs['OUTDIR'], "{}_K{}.json".format(self.obsname.replace('/', '_'), self.kernel))
+            self.outfile = os.path.join(kwargs['OUTDIR'], "{}_K{}_S{}.json".format(self.obsname.replace('/', '_'),
+                                                                                self.kernel,self.SEED))
         elif self.buildtype=="savedparam":
             self.paramsavefile = kwargs['PARAMFILE']
             import json
@@ -45,9 +47,9 @@ class GaussianProcess():
                   "This code does not support multi output GP")
             exit(1)
         if self.buildtype == 'data':
-            self.model = self.buildGPmodelFromData()
+            self.modely,self.modelz = self.buildGPmodelFromData()
         elif self.buildtype == 'savedparam':
-            self.model = self.buildGPmodelFromSavedParam()
+            self.modely,self.modelz = self.buildGPmodelFromSavedParam()
 
     def approxmeancountval(self, x):
         return self.meanappset.vals(x)[0]
@@ -82,45 +84,131 @@ class GaussianProcess():
         import json
         Ntr = int((1-self.keepout) *self.nens)
         Ns = 25
-        seed = 992739462
-        np.random.seed(seed)
+
+        np.random.seed(self.SEED)
+
         Xtrindex = np.random.choice(np.arange(self.nens), Ntr, replace=False)
         Xtr = np.repeat(self.X[Xtrindex, :], [Ns] * len(Xtrindex), axis=0)
+
         MCtr = np.repeat(self.MC[Xtrindex], Ns)
+
         DeltaMCtr = np.repeat(self.DeltaMC[Xtrindex], Ns)
+        DeltaMCtr2D = np.array([DeltaMCtr]).transpose()
 
         # Get Ns samples of each of the Ntr training distribution
         Ytr = np.random.normal(MCtr, DeltaMCtr)
-        M = [self.approxmeancountval(x) for x in Xtr]
+        Mtr = np.array([self.approxmeancountval(x) for x in Xtr])
         # Y-M (Training labels)
-        Ytrmm = Ytr - M
+        Ytrmm = Ytr - Mtr
         Ytrmm2D = np.array([Ytrmm]).transpose()
 
         # Homoscedastic noise (for now) that we will find during parameter tuning
-        lik = GPy.likelihoods.Gaussian()
-
-        polyorder = None
-        if self.kernel in ['poly','or']:
-            polyorder = 3
-
-        kernelObj = self.getKernel(self.kernel,polyorder)
-
-        # 0 mean GP to model f
-        model = GPy.core.GP(Xtr,
-                            Ytrmm2D,
-                            kernel=kernelObj,
-                            likelihood=lik
-                            )
-
-        print(model.likelihood.variance)
-        print(model.kern.parameters)
-        print(model.kern.param_array)
-        print(model.param_array.tolist())
+        # lik = GPy.likelihoods.Gaussian()
         start = timer()
         print("##############################")
         print(datetime.datetime.now())
         print("##############################")
         sys.stdout.flush()
+
+        polyorder = None
+        if self.kernel in ['poly','or']:
+            polyorder = 3
+
+        database = {
+            # "savedmodelparams": model.param_array.tolist(),
+            # "param_names_flat": model.parameter_names_flat().tolist(),
+            # "param_names": model.parameter_names(),
+            "Ntr": Ntr,
+            "Ns": Ns,
+            'seed': self.SEED,
+            'kernel': self.kernel,
+            "keepout": self.keepout * 100,
+            "obsname": self.obsname,
+            "Xtrindex": Xtrindex.tolist(),
+            "Ytrmm": Ytrmm.tolist(),
+            'modely':{},
+            'modelz':{},
+            'log':{'stopcond':self.STOPCOND}
+        }
+        if self.kernel in ['poly', 'or']:
+            database['polyorder'] = polyorder
+
+        kernelObjZero = self.getKernel(self.kernel, polyorder)
+        modelzero = GPy.models.GPHeteroscedasticRegression(Xtr,
+                                                       Ytrmm2D,
+                                                       kernel=kernelObjZero
+                                                    )
+        modelzero['.*het_Gauss.variance'] =  DeltaMCtr2D
+        modelzero.het_Gauss.variance.fix()
+        modelzero.optimize_restarts(num_restarts=self.nrestart,
+                                robust=True)
+        currenthetobjective = modelzero.objective_function()
+        oldhetobjective = np.infty
+        modely = modelzero
+        iteration = 0
+        bestobjectivey = np.infty
+        bestmodely = None
+        bestmodelz = None
+        while (currenthetobjective-oldhetobjective)**2 > self.STOPCOND:
+            Ybar = modely._raw_predict(Xtr)[0]
+            Ymean = np.array([y[0] for y in Ybar])
+            Ymean += Mtr
+            Vtr = (Ytr - Ymean)**2
+            Vtr = Vtr.reshape(Ntr, Ns).sum(axis=1) / Ns
+            Ztr = [np.log(v) for v in Vtr]
+            Ztr2D = np.array([Ztr]).transpose()
+            kernelObjz = self.getKernel(self.kernel, polyorder)
+            modelz = GPy.models.GPRegression(
+                                self.X[Xtrindex, :],
+                                Ztr2D,
+                                kernel=kernelObjz,
+                                normalizer = True
+                                )
+            modelz.optimize_restarts(num_restarts=self.nrestart,
+                                     robust=True)
+            Zbar,Zv = modelz.predict(Xtr)
+            Zmean = np.array([z[0] for z in Zbar])
+            Zvar = np.array([z[0] for z in Zv])
+            Vmean = np.exp(Zmean + (Zvar / 2))
+            Vmean2D = np.array([Vmean]).transpose()
+
+            kernelObjy = self.getKernel(self.kernel, polyorder)
+            modely = GPy.models.GPHeteroscedasticRegression(Xtr,
+                                                            Ytrmm2D,
+                                                            kernel=kernelObjy
+                                                            )
+            modely['.*het_Gauss.variance'] = Vmean2D
+            modely.het_Gauss.variance.fix()
+            modely.optimize_restarts(num_restarts=self.nrestart,
+                                        robust=True)
+            oldhetobjective = currenthetobjective
+            currenthetobjective = modely.objective_function()
+            print((currenthetobjective - oldhetobjective) ** 2)
+            sys.stdout.flush()
+            if bestobjectivey > modely.objective_function():
+                bestobjectivey = modely.objective_function()
+                bestmodely = modely
+                bestmodelz = modelz
+
+            if iteration==0:
+                database['modely']['param_names_flat'] = modely.parameter_names_flat().tolist()
+                database['modelz']['param_names_flat'] = modelz.parameter_names_flat().tolist()
+                database['modely']["param_names"] = modely.parameter_names()
+                database['modelz']["param_names"] = modelz.parameter_names()
+                database['modely']['savedmodelparams'] = []
+                database['modelz']['savedmodelparams'] = []
+                database['modely']['objective'] = []
+                database['modelz']['objective'] = []
+            database['modely']['savedmodelparams'].append(modely.param_array.tolist())
+            database['modelz']['savedmodelparams'].append(modelz.param_array.tolist())
+            database['modely']['objective'].append(modely.objective_function())
+            database['modelz']['objective'].append(modelz.objective_function())
+            database['log']['iterations_done'] = iteration
+            database['log']['timetaken'] = timer()-start
+            iteration+=1
+            with open(self.outfile, 'w') as f:
+                json.dump(database, f, indent=4)
+
 
         # if self.nprocess > 1:
         #     print("Something is wrong with parallel runs. FIX required\nQuitting for now")
@@ -134,38 +222,12 @@ class GaussianProcess():
         #
         # else:
 
-        model.optimize()
-        model.optimize_restarts(num_restarts=self.nrestart,
-                                robust=True)
-        print(timer() - start)
-        print(model.likelihood.variance)
-        print(model.kern.param_array)
-        print(model.param_array.tolist())
-        print(model)
+
         print("##############################")
         print(datetime.datetime.now())
         print("##############################")
         sys.stdout.flush()
-
-        data = {
-            "savedmodelparams":model.param_array.tolist(),
-            "param_names_flat": model.parameter_names_flat().tolist(),
-            "param_names": model.parameter_names(),
-            "Ntr": Ntr,
-            "Ns": Ns,
-            'seed': seed,
-            'kernel':self.kernel,
-            "keepout":self.keepout*100,
-            "obsname":self.obsname,
-            "Xtrindex": Xtrindex.tolist(),
-            "Ytrmm": Ytrmm.tolist()
-        }
-        if self.kernel in ['poly','or']:
-            data['polyorder'] = polyorder
-        with open(self.outfile, 'w') as f:
-            json.dump(data, f, indent=4)
-
-        return model
+        return bestmodely,bestmodelz
 
     def  buildGPmodelFromSavedParam(self):
         import json
@@ -239,6 +301,12 @@ if __name__ == "__main__":
                                choices=["matern32", "matern52","sqe","ratquad","poly","or"],
                                help="Kernel to use (ARD will be set to True for all (where applicable)\n"
                                     "REQUIRED only build type (\"-b\", \"--buildtype\") is \"data\"")
+    parser.add_argument('--stopcond', dest="STOPCOND", default=10 **-4, type=float,
+                        help="Stoping condition metric\n"
+                                    "REQUIRED only build type (\"-b\", \"--buildtype\") is \"data\"")
+    parser.add_argument('-s','--seed', dest="SEED", default=6391273, type=int,
+                        help="Seed (Control for n-fold crossvalidation)\n"
+                             "REQUIRED only build type (\"-b\", \"--buildtype\") is \"data\"")
 
 
     parser.add_argument("-p", "--paramfile", dest="PARAMFILE", type=str, default=None,
@@ -259,6 +327,8 @@ if __name__ == "__main__":
         KEEPOUT=args.KEEPOUT,
         OUTDIR=args.OUTDIR,
         APPROX=args.APPROX,
+        SEED=args.SEED,
+        STOPCOND=args.STOPCOND,
         PARAMFILE=args.PARAMFILE
     )
 
