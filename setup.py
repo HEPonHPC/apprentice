7108f2899b66c89e7c8c6cca4a99b00b21d886cc
Mohan Krishnamoorthy <mkrishnamoorthy@anl.gov>
2020-06-03 09:46:03 -0500

    Adding GPy to setup

--- setup.py
+++ setup.py
@@ -16,7 +16,8 @@ setup(
    'h5py>=2.8.0',
    'mpi4py>=3.0.0',
    'matplotlib>=3.0.0',
-   'pyDOE2>=1.3.0'
+   'pyDOE2>=1.3.0',
+   'GPy>=1.9.9'
  ],
   scripts=["bin/app-ls", "bin/app-tune2", "bin/app-build", "bin/app-predict", "bin/app-datadirtojson", "bin/app-yoda2h5", "bin/app-yodaenvelope", "bin/app-sample", "etc/extrema.py"],
   extras_require = {
