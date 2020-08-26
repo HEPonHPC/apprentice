aa37571f20f3471107d8366d49d532e38608fa28
Holger Schulz <hschulz@fnal.gov>
2020-08-26 15:34:29 -0500

    Gradient test and multi point gradient recurrence test

--- test-restructure/testfunctionalapprox.py
+++ test-restructure/testfunctionalapprox.py
@@ -14,7 +14,7 @@ class PolynomialTestSingle(unittest.TestCase):
         ft = apprentice.FunctionalApprox(2, m=3, n=0)
         ft.setCoefficients(aps._PC)
         ft.setScaler(aps._SCLR.asDict)
-        self.assertEqual(np.sum(aps.vals([0,1])-ft.vals([0,1]).ravel()), 0)
+        self.assertEqual(np.sum(aps.vals([0,1])-ft.val([0,1]).ravel()), 0)
 
 class RationalTestSingle(unittest.TestCase):
     def test(self):
@@ -22,7 +22,7 @@ class RationalTestSingle(unittest.TestCase):
         ft = apprentice.FunctionalApprox(2, m=1, n=3)
         ft.setCoefficients(aps._PC, aps._QC)
         ft.setScaler(aps._SCLR.asDict)
-        self.assertEqual(np.sum(aps.vals([0,1]) - ft.vals([0,1]).ravel()), 0)
+        self.assertEqual(np.sum(aps.vals([0,1]) - ft.val([0,1]).ravel()), 0)
 
 class PolynomialTestMulti(unittest.TestCase):
     def test(self):
@@ -37,7 +37,7 @@ class PolynomialTestMulti(unittest.TestCase):
         AR = np.zeros((1793,NTEST))
         for num, x in enumerate(X):
             AR[:,num] = aps.vals(x)
-        assert abs(np.sum(AR-ft.vals(X))) < 1e-9
+        assert abs(np.sum(AR-ft.val(X))) < 1e-9
 
 
 class RationalTestMulti(unittest.TestCase):
@@ -53,7 +53,64 @@ class RationalTestMulti(unittest.TestCase):
         AR = np.zeros((254,NTEST))
         for num, x in enumerate(X):
             AR[:,num] = aps.vals(x)
-        assert abs(np.sum(AR-ft.vals(X))) < 1e-9
+        assert abs(np.sum(AR-ft.val(X))) < 1e-9
+
+
+class PolynomialTestGrad(unittest.TestCase):
+    def test(self):
+        aps = readAppSet("test-restructure/la_30.json")
+        ft = apprentice.FunctionalApprox(2, m=3, n=0)
+        ft.setCoefficients(aps._PC)
+        ft.setScaler(aps._SCLR.asDict)
+        self.assertEqual(np.sum(aps.grads([0,1]) -ft.grad([0,1])), 0)
+
+
+
+
+class PolynomialTestGradMulti(unittest.TestCase):
+    def test(self):
+        aps = readAppSet("test-restructure/la_30.json")
+        ft = apprentice.FunctionalApprox(2, m=3, n=0)
+        ft.setCoefficients(aps._PC)
+        ft.setScaler(aps._SCLR.asDict)
+
+        NTEST=20
+        X = np.random.rand(NTEST,2)
+
+        AGR = np.zeros((NTEST,1793,2))
+        for num, x in enumerate(X):
+            AGR[num] = aps.grads(x)
+
+        self.assertEqual(np.sum(aps.grads([0,1]) -ft.grad([0,1])), 0)
+
+class GradientRecurrenceTest(unittest.TestCase):
+    def test(self):
+        aps = readAppSet("test-restructure/la_30.json")
+        ft = apprentice.FunctionalApprox(2, m=3, n=0)
+        ft.setCoefficients(aps._PC)
+        ft.setScaler(aps._SCLR.asDict)
+        NTEST=2000
+        X = np.random.rand(NTEST,2)
+
+
+        import time
+        t0 = time.time()
+        sgrd = np.zeros((NTEST, 2, 10))
+        for num, x in enumerate(X):
+            sgrd[num] = apprentice.functionalapprox.gradientRecurrence(x, ft.structure_, ft.scaler_.jacfac, ft.nonzerostruct_, ft.reducedstruct_)
+        t1 = time.time() - t0
+        self.assertEqual(np.sum(sgrd -apprentice.functionalapprox.gradientRecurrenceMulti(X, ft.structure_, ft.scaler_.jacfac, ft.nonzerostruct_, ft.reducedstruct_) ), 0)
+
+        import time
+        t0 = time.time()
+        sgrd = np.zeros((NTEST, 2, 10))
+        for num, x in enumerate(X):
+            sgrd[num] = apprentice.functionalapprox.gradientRecurrence(x, ft.structure_, ft.scaler_.jacfac, ft.nonzerostruct_, ft.reducedstruct_)
+        t1 = time.time() - t0
+        t0 = time.time()
+        apprentice.functionalapprox.gradientRecurrenceMulti(X, ft.structure_, ft.scaler_.jacfac, ft.nonzerostruct_, ft.reducedstruct_)
+        t2 = time.time() - t0
+        print("{} vs {}".format(t1,t2))
 
 if __name__ == '__main__':
 
