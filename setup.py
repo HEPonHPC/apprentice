15ba2f2f4f33fe1451b4466c21e29bb82b1b8004
Holger Schulz <hschulz@fnal.gov>
2020-09-14 13:21:13 -0500

    add packaging module to list of requirements --- needed for version checks

--- setup.py
+++ setup.py
@@ -17,7 +17,8 @@ setup(
    'mpi4py>=3.0.0',
    'matplotlib>=3.0.0',
    'pyDOE2>=1.3.0',
-   'GPy>=1.9.9'
+   'GPy>=1.9.9',
+   'packaging'
  ],
   scripts=["bin/app-ls", "bin/app-tune2", "bin/app-build", "bin/app-predict", "bin/app-datadirtojson", "bin/app-yoda2h5", "bin/app-yodaenvelope", "bin/app-sample", "etc/extrema.py"],
   extras_require = {
